67936d832b7e2e8fdd678318904b6c4614af3b38
Karo <76846752+InvalidKaro@users.noreply.github.com>
2023-11-04 20:23:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup_info = {
-    "name": "pyblox",
+    "name": "py3blox",
     "version": "2.0.0",
     "author": "invalidkaro",
     "author_email": "mail@invalidkaro.dev",
